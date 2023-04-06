ce11a504cd519d029fd787cbf9098759f15a2bbb
Afrodyta Różycka <afrodyta.rozycka@intive.com>
2023-04-06 13:15:58 +0200

    flexbox kno

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        height: "100%",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "#ff0000",
+          justifyContent: "center",
+          width: 10,
+          height: 100,
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 10,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          width: 10,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
